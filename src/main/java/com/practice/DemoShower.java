353a1c3cbd27f1feafc72890c0a3b3e12eea5739
hejiangle <jlhe@thoughtworks.com>
2017-07-20 21:57:14 +0800

    [Traingle-02] finish Diamond Exercises.

---
+++ src/main/java/com/practice/DemoShower.java
@@ -0,0 +1,61 @@
+package com.practice;
+
+public class DemoShower {
+
+    public static void main(String[] args) {
+
+//        drawStar();
+//        drawHorizontalLine(8);
+//        drawVerticalLine(3);
+//        drawRightTraingle(3);
+        drawIsoscelesTraingle(4);
+    }
+
+    private static void drawIsoscelesTraingle(int n) {
+        for(int h = 0; h < n; h++){
+            blank(n - h - 1);
+            drawHorizontalLine(2 * (h + 1) - 1);
+            toNextLine();
+        }
+        for(int h = n - 2; h >= 0; h-- ){
+            blank(n - h - 1);
+            drawHorizontalLine(2 * (h + 1) - 1);
+            toNextLine();
+        }
+    }
+
+    private static void blank(int i) {
+        for(int j = 0; j < i; j++){
+            System.out.print(" ");
+        }
+    }
+
+    private static void drawRightTraingle(int line) {
+        for(int h = 0; h < line; h++){
+            drawHorizontalLine(h + 1);
+            toNextLine();
+        }
+    }
+
+    private static void drawVerticalLine(int i) {
+        for(int j = 0; j < i; j++){
+            drawStar();
+            toNextLine();
+        }
+    }
+
+    private static void toNextLine() {
+        System.out.println();
+    }
+
+    private static void drawHorizontalLine(int i) {
+        for(int j = 0; j < i; j++){
+            drawStar();
+        }
+    }
+
+    private static void drawStar() {
+        System.out.print("*");
+    }
+
+}
